b6cd32799cdedfb4da297c0bbda41bf586d36359
Ithndr <tm21999@gmail.com>
2022-12-21 11:34:29 -0500

    better organization and some functionallity added

---
+++ src/components/index.js
@@ -0,0 +1,95 @@
+import ReactDOM from 'react-dom/client';
+import React, { useState, useEffect } from 'react';
+import { HashRouter, Routes, Route, Link} from 'react-router-dom';
+import Login from './Login';
+
+
+const App = ()=> {
+  const [registerUsername, setRegisterUsername] = useState('');
+  const [registerPassword, setRegisterPassword] = useState('');
+  const [loginUsername, setLoginUsername] = useState('');
+  const [loginPassword, setLoginPassword] = useState('');
+  const [user, setUser] = useState({});
+
+  const exchangeTokenForUser = ()=> {
+    const token = window.localStorage.getItem('token');
+    if(token){
+      fetch('https://strangers-things.herokuapp.com/api/2209-FTB-ET-WEB-AM/users/me', {
+        headers: {
+        'Content-Type': 'application/json',
+        'Authorization': `Bearer ${ token }` 
+        },
+      })
+      .then(response => response.json())
+      .then(result => {
+        const user = result.data;
+        setUser(user);
+      })
+      .catch(err => console.log(err));
+    }
+  };
+  useEffect(()=> {
+    exchangeTokenForUser();
+  }, []);
+
+  const register = (ev)=> {
+    ev.preventDefault();
+    fetch('https://strangers-things.herokuapp.com/api/2209-FTB-ET-WEB-AM/users/register', {
+      method: "POST",
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({
+        user: {
+          username: registerUsername,
+          password: registerPassword 
+        }
+      })
+    })
+    .then(response => response.json())
+    .then(result => {
+      if(!result.success){
+        throw result.error;
+      }
+      console.log(result);
+
+    })
+    .catch( err => console.log(err));
+  }
+
+  const logout = ()=> {
+    window.localStorage.removeItem('token');
+    setUser({});
+  }
+
+  return (
+    <div>
+      <h1>Profs Auth App</h1>
+      {
+        user._id ? <div>Welcome { user.username } <button onClick={ logout }>Logout</button></div> : null
+      }
+      {
+        !user._id ? (
+      <div>
+        <form onSubmit = { register }>
+          <input
+            placeholder='username'
+            value={ registerUsername }
+            onChange = { ev => setRegisterUsername(ev.target.value)}
+          />
+          <input 
+            placeholder='password'
+            value={ registerPassword }
+            onChange = { ev => setRegisterPassword(ev.target.value)}
+          />
+          <button>Register</button>
+        </form>
+        <Login exchangeTokenForUser={ exchangeTokenForUser } />
+      </div>) : null
+      } 
+    </div>
+
+  );
+};
+const root = ReactDOM.createRoot(document.querySelector('#root'));
+root.render(<HashRouter><App /></HashRouter>);
--- src/index.js
+++ src/index.js
@@ -1,27 +1,35 @@
 import ReactDOM from 'react-dom';
 import React, { useState, useEffect } from 'react';
 import { HashRouter, Routes, Route, Link} from 'react-router-dom';
-
+import posts from './posts';
 
 const App = ()=> {
   const [posts, setPosts] = useState([]);
   
-
-  
   useEffect(()=> {
+    const fetchPosts = async () =>{
+      const response = await fetch( 'https://strangers-things.herokuapp.com/api/2209-ftb-et-web-am/posts');
+      const data = await response.json();
+      setPosts(data);
+    }
   }, [])
+
+
+
   return (
     <div>
       <h1>Strangers Things</h1>
       <nav>
+        <Link to='/home'>Home</Link>
         <Link to='/posts'>Posts ({posts.length})</Link>
         <Link to='/login'>Login</Link>
         <Link to='/register'>Register</Link>
       </nav>
       <Routes>
-        <Route path='/posts' element= { <div>Posts</div>}/>} />
-        <Route path='/login' element={ <div>Login</div>} />} />
-        <Route path='/register' element={ <div>Register</div>} />} />
+        <Route path='/home' element={ <div>Home</div>}/>
+        <Route path='/posts' element= { <div>Posts</div>}/>
+        <Route path='/login' element={ <div>Login</div>} />
+        <Route path='/register' element={ <div>Register</div>} />
       </Routes> 
     </div>
 
